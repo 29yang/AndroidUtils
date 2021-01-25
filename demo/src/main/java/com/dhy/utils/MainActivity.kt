de11dc5210b8b88d070bbc5715596d6263bbc134
Dong Hongyang <donghongyang@nissanchina.cn>
2021-01-25 14:47:33 +0800

    0

--- demo/src/main/java/com/dhy/utils/MainActivity.kt
+++ demo/src/main/java/com/dhy/utils/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.dhy.utils
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dhy.utilslibrary.logger.LoggerUtil
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var dimension = resources.getDimension(R.dimen.dp_10)
+        val dm2 = resources.displayMetrics
+        println("Loggerdensity : " + dm2.density)
+        println("LoggerdensityDPI : " + dm2.densityDpi)
+        println("LoggerUtilheigth2 : " + dm2.heightPixels)
+        println("LoggerUtilwidth2 : " + dm2.widthPixels)
+        LoggerUtil.e("${dimension/dm2.density}")
     }
 }
